6caf012f7c973e529aaf2ac54b4052ccc59609b6
z7087 <75190047+z7087@users.noreply.github.com>
2025-01-12 20:17:41 +0800

    小修

--- src/main/java/me/z7087/blockminer/mixin/MixinClientPlayerInteractionManager.java
+++ src/main/java/me/z7087/blockminer/mixin/MixinClientPlayerInteractionManager.java
@@ -1,21 +1,14 @@
 package me.z7087.blockminer.mixin;
 
 import me.z7087.blockminer.BlockMinerMod;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public abstract class MixinClientPlayerInteractionManager {
-    @Shadow
-    @Final
-    private MinecraftClient client;
-
     @Inject(method = "tick", at = @At(value = "RETURN"))
     private void onTick(CallbackInfo ci) {
         BlockMinerMod.INSTANCE.taskManager.tick();
