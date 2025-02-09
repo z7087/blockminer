2074040ad995554f28f4603a95bf8cdc09f0482c
z7087 <75190047+z7087@users.noreply.github.com>
2025-02-09 18:06:46 +0800

    小修

--- src/main/java/me/z7087/blockminer/mixin/minecraft/network/MixinClientConnection.java
+++ src/main/java/me/z7087/blockminer/mixin/minecraft/network/MixinClientConnection.java
@@ -3,7 +3,6 @@ package me.z7087.blockminer.mixin.minecraft.network;
 import me.z7087.blockminer.util.BlinkUtils;
 import net.minecraft.network.ClientConnection;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
