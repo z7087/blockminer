9db8d7003948268d9851f711ede2e1617e54f172
z7087 <75190047+z7087@users.noreply.github.com>
2025-06-25 11:19:37 +0800

    适配1.21.6

--- src/main/java/me/z7087/blockminer/mixin/minecraft/network/MixinClientConnection.java
+++ src/main/java/me/z7087/blockminer/mixin/minecraft/network/MixinClientConnection.java
@@ -4,13 +4,20 @@ import me.z7087.blockminer.util.BlinkUtils;
 import net.minecraft.network.ClientConnection;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ClientConnection.class)
 public class MixinClientConnection {
     //#if MC >= 12002
 
-    @ModifyVariable(method = "send(Lnet/minecraft/network/packet/Packet;Lnet/minecraft/network/PacketCallbacks;Z)V", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    @org.spongepowered.asm.mixin.injection.ModifyVariable(method = "send("
+            + "Lnet/minecraft/network/packet/Packet;"
+            //#if MC >= 12106
+            //$$ + "Lio/netty/channel/ChannelFutureListener;"
+            //#else
+            + "Lnet/minecraft/network/PacketCallbacks;"
+            //#endif
+            + "Z"
+            + ")V", at = @At("HEAD"), ordinal = 0, argsOnly = true)
     private boolean modifyFlushArg(boolean value) {
         // 转Object不需要check-cast字节码，转ClientConnection需要
         return value && ((Object) BlinkUtils.blinkingConnection.get()) != this;
